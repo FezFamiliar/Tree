2c083ad0dde40fa54dac01e47b2df4c5c25095fe
root <root@localhost.localdomain>
2019-11-07 18:11:15 +0000

    height functions done

--- main.c
+++ main.c
@@ -114,16 +114,31 @@ void Preorder(struct Node* start){
 	Preorder(start->right);
 
 }
+
+int Height(struct Node* root){
+
+
+	if(root == NULL) return 0;
+	else{
+
+	int lh = Height(root->left);
+	int rh = Height(root->right);
+
+	if(lh > rh) return(lh + 1);
+	else return(rh + 1);
+
+	}
+}
+
+
 int main(){
 
 	struct Node* root = NULL;
 	root = Insert(root,5);
 	root = Insert(root,10);
-	root = Insert(root,2);
-	root = Insert(root,1);
-	root = Insert(root,7);
-	root = Insert(root,50);
-	root = RemoveNode(root,2);
-	Preorder(root);
+	root = Insert(root,15);
+	root = Insert(root,13);
+	printf("left subtree height: %d\n",Height(root->left));
+	printf("Right subtree height: %d",Height(root->right));
 	return 0;
 }
