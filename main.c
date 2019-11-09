33050f64651ce5f344135fa0c76a669a975d0ed4
root <root@localhost.localdomain>
2019-11-09 11:55:50 +0000

    deleted function declarations in.c

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 
 #include "tree.h"
 
-void Preorder(struct Node* root);
-struct Node* Insert(struct Node* root,int data);
+
+
 int main(){
 
 	struct Node* root = NULL;
@@ -12,7 +12,6 @@ int main(){
     	root = Insert(root, 40);
     	root = Insert(root, 50);
     	root = Insert(root, 25);
-	root = Insert(root,50);
 	Preorder(root);
 	return 0;
 
